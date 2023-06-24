c015570aada0c52bdbb666c17b3b92326e519d37
Briton Mhitike <101688136+Briton-james@users.noreply.github.com>
2023-06-24 21:35:46 +0300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace britonforms.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public bool hasData = false;
+        public string first_name = "";
+        public string last_name = "";
+        public string feedback = "";
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost(string message)
+        {
+            hasData = true;
+            first_name = Request.Form["first_name"];
+            last_name = Request.Form["last_name"];
+            feedback = Request.Form["feedback"];
+
+        }
+    }
+}
